dbf68e7a4dfb70bc41b205c24daacd9c5fff112c
Dion Pettaway <dionp@fullsail.edu>
2020-01-16 08:19:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Visualizer from './Components/Visualizer/Visualizer'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Visualizer></Visualizer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Visualizer/Visualizer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Node from '../Node/Node'
+
+import './Visualizer.css'
+
+class Visualizer extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Visualizer;
